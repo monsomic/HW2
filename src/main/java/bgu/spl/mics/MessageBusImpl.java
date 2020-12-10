e806cfdc6f5f68dfdb680d3bbb4c0ab76c85df2f
monsomic <73487036+monsomic@users.noreply.github.com>
2020-12-10 14:02:13 +0200

    Add files via upload

--- src/main/java/bgu/spl/mics/MessageBusImpl.java
+++ src/main/java/bgu/spl/mics/MessageBusImpl.java
@@ -23,6 +23,7 @@ public class MessageBusImpl implements MessageBus {
 
 	private ConcurrentHashMap<Event,Future> futureMap; // hashmap (some Event (event<T>),its future (future<T>)
 
+	private Object  lockUnregisterSendEvent, lockUnregisterSendBroadcast;
 
 
 
@@ -38,24 +39,26 @@ public class MessageBusImpl implements MessageBus {
 	
 	@Override
 	public <T> void subscribeEvent(Class<? extends Event<T>> type, MicroService m) {    //check if the type exists
+		// sync - because : two MS subscribe to same type of event - makes two identical event types in map
+		synchronized ((type)) {
+			ConcurrentLinkedQueue Q = messageTypeMap.get(type);
+			if (Q == null)
+				messageTypeMap.put(type, new ConcurrentLinkedQueue<MicroService>());
 
-		ConcurrentLinkedQueue Q = messageTypeMap.get(type);
-		if (Q==null)
-			messageTypeMap.put(type, new ConcurrentLinkedQueue<MicroService>());
-
-		messageTypeMap.get(type).add(m);
-
+			messageTypeMap.get(type).add(m);
+		}
 	}
 
 	@Override
 	public void subscribeBroadcast(Class<? extends Broadcast> type, MicroService m) {
+		// sync - because : two MS subscribe to same type of event - makes two identical event types in map
+		synchronized ((type)) {
+			ConcurrentLinkedQueue Q = messageTypeMap.get(type);
+			if (Q == null)
+				messageTypeMap.put(type, new ConcurrentLinkedQueue<MicroService>());
 
-		ConcurrentLinkedQueue Q = messageTypeMap.get(type);
-		if (Q==null)
-			messageTypeMap.put(type, new ConcurrentLinkedQueue<MicroService>());
-
-		messageTypeMap.get(type).add(m);
-
+			messageTypeMap.get(type).add(m);
+		}
     }
 	@Override @SuppressWarnings("unchecked")
 	public <T> void complete(Event<T> e, T result) {
@@ -64,32 +67,38 @@ public class MessageBusImpl implements MessageBus {
 	}
 
 	@Override
-	public void sendBroadcast(Broadcast b) {
+	public void sendBroadcast(Broadcast b) {     //sync - becouse : inside for - get can bring null becouse m was deleted from qmap in unregister
+
 		ConcurrentLinkedQueue<MicroService> Q = messageTypeMap.get(b.getClass());
 		if(Q!=null) {
-			for(MicroService m :Q){
+			synchronized (lockSubBroadcast){
+			for(MicroService m :Q) {
 				qmap.get(m).add(b);
+				}
 			}
 		}
 	}
 
 	
 	@Override
-	public synchronized  <T> Future<T>  sendEvent(Event<T> e) { //enque to the right queue (round robin) , create future object , put in futuremap
+	public  <T> Future<T>  sendEvent(Event<T> e) { //enque to the right queue (round robin) , create future object , put in futuremap
+		// sync - because : we can pass first if and then try poll from an empty Q because unregister by another thread :::::BUT - in our implementation this cant happen. sync or  not ?
+		//		  because : if two MS send the same type of event and there is one MS in that Q(in messageTtpeMap) :::BUT - cant happen in our system
 
-		ConcurrentLinkedQueue<MicroService> Q = messageTypeMap.get(e.getClass());
+		synchronized (lockUnregisterSendEvent) {
+			ConcurrentLinkedQueue<MicroService> Q = messageTypeMap.get(e.getClass());
+			if (Q == null || Q.isEmpty())
+				return null;
 
-		if(Q==null || Q.isEmpty())
-			return null;
+			MicroService firstM = Q.poll();
+			qmap.get(firstM).add(e);
+			messageTypeMap.get(e.getClass()).add(firstM); // for round robin
 
-		MicroService firstM = Q.poll();
-		qmap.get(firstM).add(e);
-		messageTypeMap.get(e.getClass()).add(firstM); // for round robin
-
-		Future<T> f= new Future<>();
-		futureMap.put(e,f);
-		notifyAll(); // check if needed here and does it work
-		return f;
+			Future<T> f = new Future<>();
+			futureMap.put(e, f);
+			//notifyAll(); //no need becouse blockingQueue notifies alone.
+			return f;
+		}
 	}
 
 	@Override
@@ -104,15 +113,18 @@ public class MessageBusImpl implements MessageBus {
 	public void unregister(MicroService m) {
 		// delete from qmap
 		//search in messageTypeMap and delete microservice from it
-		qmap.remove(m);
-		for(Class<? extends Message> type :messageTypeMap.keySet()){
-			for(MicroService curr: messageTypeMap.get(type)){
-				if(curr.equals(m))
-					messageTypeMap.get(type).remove(curr);
+		synchronized (lockUnregisterSendBroadcast){
+		synchronized ((lockUnregisterSendEvent)) {
+
+			qmap.remove(m);
+			for (Class<? extends Message> type : messageTypeMap.keySet()) {
+				for (MicroService curr : messageTypeMap.get(type)) {
+					if (curr.equals(m))
+						messageTypeMap.get(type).remove(curr);
+				}
+			}
 			}
 		}
-
-
 	}
 
 	@Override
