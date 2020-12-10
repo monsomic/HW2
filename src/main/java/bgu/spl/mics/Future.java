07a638f66e8a420c7def9c04e7deacc5e48f7555
monsomic <73487036+monsomic@users.noreply.github.com>
2020-12-10 17:33:02 +0200

    Add files via upload

--- src/main/java/bgu/spl/mics/Future.java
+++ src/main/java/bgu/spl/mics/Future.java
@@ -65,7 +65,7 @@ public class Future<T> {
      * 	       wait for {@code timeout} TimeUnits {@code unit}. If time has
      *         elapsed, return null.
      */
-	public T get(long timeout, TimeUnit unit) {
+	public synchronized T get(long timeout, TimeUnit unit) {
 		while (!isDone){
 			try{ wait(unit.toMillis(timeout));}
 			catch(InterruptedException e){e.getStackTrace();}
